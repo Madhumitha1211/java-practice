08a8ea32516d4fa43cbc781d9ffeb264f83945b2
Madhumitha1211 <madhumitha6161@gmail.com>
2026-02-03 20:32:19 +0530

    Day 2: Added VariablesExample.java

---
+++ VariablesExample.java
@@ -0,0 +1,16 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 20;
+        double avg = 8.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name ="Madhu";
+
+        System.out.println("My Name is : "+ name);
+        System.out.println("My Age is : " + age);
+        System.out.println("My Average Mark is : "+ avg);
+        System.out.println("My Grade is : "+ grade);
+        System.out.println("Is she a Student ? " + isStudent);
+    }
+    
+}
