1fe4ebe9e170eeffa4272aed94a7eef099f675c6
Madhumitha1211 <madhumitha6161@gmail.com>
2026-02-04 20:56:42 +0530

    Day 3: Added ArithmeticExample.java

---
+++ ArithmeticExample.java
@@ -0,0 +1,19 @@
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        int num1 = 21;
+        int num2 = 52;
+
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        double division = num1 / num2;
+        int modulus = num1 % num2;
+
+        System.out.println("Addition : " + addition);
+        System.out.println("Subtraction : " + subtraction);
+        System.out.println("Multiplication : " + multiplication);
+        System.out.println("Division : " + division);
+        System.out.println("Modulus : " + modulus);
+    }
+    
+}
