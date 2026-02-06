045b2982cee4e3dd44e236d86fce1c0c7f279e41
Madhumitha1211 <madhumitha6161@gmail.com>
2026-02-06 19:49:38 +0530

    Day 5: Student grade program using if-else-if

---
+++ StudentGrade.java
@@ -0,0 +1,23 @@
+public class StudentGrade {
+    public static void main(String[] args) {
+        int mark = 82;
+        System.out.println("Mark : " +mark);
+        
+
+        if(mark >= 90){
+            System.out.println("Grade : A");
+        }
+        else if(mark >= 75){
+            System.out.println("Grade : B");
+
+        }
+        else if (mark >=50){
+            System.out.println("Grade : C");
+        }
+        else {
+            System.out.println("Fail");
+        }
+
+    }
+    
+}
